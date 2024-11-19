bcdbfd99bb25259539695106825324f7b2fe193c
Jad Bizri <jb4954@hotmail.com>
2024-11-19 13:43:56 -0500

    Added get Id

--- src/main/java/com/example/fetch_assignment/model/Receipt.java
+++ src/main/java/com/example/fetch_assignment/model/Receipt.java
@@ -20,6 +20,10 @@ public class Receipt {
         this.items = items;
     }
 
+    public String getId() {
+        return id;
+    }
+
     public void setId(String id) {
         this.id = id;
     }
