df91c0c0d3f6307587444045550508e4b16f056e
Jad Bizri <jb4954@hotmail.com>
2024-11-19 13:42:25 -0500

    Converted Receipt from record to class

--- src/main/java/com/example/fetch_assignment/model/Receipt.java
+++ src/main/java/com/example/fetch_assignment/model/Receipt.java
@@ -2,6 +2,27 @@ package com.example.fetch_assignment.model;
 
 import java.util.List;
 
-public record Receipt (String id, String retailer, String purchaseDate, String purchaseTime, String total, List<Item> items ){
+public class Receipt {
+
+    private String id;
+    private String retailer;
+    private String purchaseDate;
+    private String purchaseTime;
+    private String total;
+    private List<Item> items;
+
+    public Receipt (String id, String retailer, String purchaseDate, String purchaseTime, String total, List<Item> items) {
+        this.id = id;
+        this.retailer = retailer;
+        this.purchaseDate = purchaseDate;
+        this.purchaseTime = purchaseTime;
+        this.total = total;
+        this.items = items;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public static record Item (String shortDescription, String price){}
 }
