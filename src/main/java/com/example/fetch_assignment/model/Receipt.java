8da1c1cb4b23b9be72eede97998ed1c85b7f90ab
Jad Bizri <jb4954@hotmail.com>
2024-11-19 02:56:20 -0500

    Changed id type from long to String

--- src/main/java/com/example/fetch_assignment/model/Receipt.java
+++ src/main/java/com/example/fetch_assignment/model/Receipt.java
@@ -2,6 +2,6 @@ package com.example.fetch_assignment.model;
 
 import java.util.List;
 
-public record Receipt (long id, String retailer, String purchaseDate, String purchaseTime, String total, List<Item> items ){
+public record Receipt (String id, String retailer, String purchaseDate, String purchaseTime, String total, List<Item> items ){
     public static record Item (String shortDescription, String price){}
 }
