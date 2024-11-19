ec7481c5ba606bb8429f43e719f26790fcc4f015
Jad Bizri <jb4954@hotmail.com>
2024-11-19 02:11:41 -0500

    init

---
+++ src/test/java/com/example/fetch_assignment/FetchAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fetch_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
