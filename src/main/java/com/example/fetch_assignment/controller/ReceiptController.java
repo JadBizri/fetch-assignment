a1a4896e6e89df54bfd34ddcacadff37a97ed173
Jad Bizri <jb4954@hotmail.com>
2024-11-19 12:24:24 -0500

    Completed controller and tested both actions using postman

--- src/main/java/com/example/fetch_assignment/controller/ReceiptController.java
+++ src/main/java/com/example/fetch_assignment/controller/ReceiptController.java
@@ -1,16 +1,31 @@
 package com.example.fetch_assignment.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.fetch_assignment.model.Receipt;
+import com.example.fetch_assignment.service.ReceiptService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 public class ReceiptController {
 
-    @GetMapping("/receipts/{id}/points")
-    public Map<String, Integer> getPoints(@PathVariable long id) {
+    private final ReceiptService receiptService;
 
+    public ReceiptController(ReceiptService receiptService) {
+        this.receiptService = receiptService;
+    }
+
+    @GetMapping("/receipts/{id}/points")
+    public Map<String, Integer> getPoints(@PathVariable String id) {
+        // (OPTIONAL) TODO: validate id to be in the form of a UUID (i.e. not '1' or '2')
+        int points = receiptService.getPoints(id);
+        return Map.of("points", points);
+    }
+
+    @PostMapping("/receipts/process")
+    public Map<String, String> processReceipt(@RequestBody Receipt receipt) {
+        // TODO: validate receipt
+        String id = receiptService.processReceipt(receipt);
+        return Map.of("id", id);
     }
 }
