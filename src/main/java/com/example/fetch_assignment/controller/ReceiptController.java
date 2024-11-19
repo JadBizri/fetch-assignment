f9ed0c631b5ba240c28e4938b4f9e5a837c20c31
Jad Bizri <jb4954@hotmail.com>
2024-11-19 14:38:11 -0500

    UUID now gets validated

--- src/main/java/com/example/fetch_assignment/controller/ReceiptController.java
+++ src/main/java/com/example/fetch_assignment/controller/ReceiptController.java
@@ -1,16 +1,19 @@
 package com.example.fetch_assignment.controller;
 
+import com.example.fetch_assignment.exceptions.InvalidInputException;
 import com.example.fetch_assignment.model.Receipt;
 import com.example.fetch_assignment.service.ReceiptService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 @RestController
 public class ReceiptController {
 
     private final ReceiptService receiptService;
+    private final Pattern idPattern = Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
 
     public ReceiptController(ReceiptService receiptService) {
         this.receiptService = receiptService;
@@ -24,7 +27,9 @@ public class ReceiptController {
 
     @GetMapping("/receipts/{id}/points")
     public Map<String, Integer> getPoints(@PathVariable String id) {
-        // (OPTIONAL) TODO: validate id to be in the form of a UUID (i.e. not '1' or '2') and ensure id exists
+        if (!idPattern.matcher(id).matches()) {
+            throw new InvalidInputException("Invalid receipt id: " + id);
+        }
         int points = receiptService.getPoints(id);
         return Map.of("points", points);
     }
